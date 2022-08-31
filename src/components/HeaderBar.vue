7f2462a969962427ca6a3c299049c59a5fd0766f
marco <marcaux@cryptolab.net>
2022-08-31 16:25:22 +0200

    added info-window to nimiq icon

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -3,13 +3,36 @@ import IconSvg from "@/components/elements/IconSvg.vue";
 import InputSearchApi from "@/components/elements/InputSearchApi.vue";
 import ButtonFilter from "@/components/elements/ButtonFilter.vue";
 import ButtonResize from "@/components/elements/ButtonResize.vue";
+import PopupInfo from "@/components/elements/PopupInfo.vue";
+import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
+
+let infoVisible = ref<boolean>(false);
+const elInfoWindow = ref(null);
+
+onClickOutside(elInfoWindow, () => {
+  infoVisible.value = false;
+});
 </script>
 
 <template>
   <header id="the-header">
-    <IconSvg
-      iconIndex="icon-nim"
+    <button 
       class="nimiq-icon"
+    >
+      <IconSvg
+        iconIndex="icon-nim"
+        @click="() => { infoVisible = !infoVisible }"
+      />
+    </button>
+    <PopupInfo
+      ref="elInfoWindow"
+      :class="`${infoVisible ? 'visible' : ''}`"
+      :title="$t('infoWindows.cryptomap.title')"
+      :text="$t('infoWindows.cryptomap.text')"
+      :url="$t('infoWindows.cryptomap.url')"
+      :urlText="$t('infoWindows.cryptomap.urlText')"
+      icon="icon-nimiq-logo-font"
     />
     <InputSearchApi />
     <ButtonFilter />
---
+++ src/components/elements/PopupInfo.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import IconSvg from "@/components/elements/IconSvg.vue";
+
+const props = defineProps<{
+  title: string;
+  text: string;
+  url: string;
+  urlText: string;
+  icon: string
+}>();
+</script>
+
+<template>
+  <div class="info-window">
+    <div class="hover-helper"></div>
+    <div class="info-window-wrap">
+      <div v-if="props.title" class="info-window-title">{{ props.title }}</div>
+      <div v-if="props.text" class="info-window-text">{{ props.text }}</div>
+      <div v-if="props.url && props.urlText" class="info-window-link">
+        <a
+          :href="props.url"
+          :title="props.title"
+          target="_blank"
+          class="blue arrow"
+        >{{ props.urlText }}<IconSvg iconIndex="icon-link-out" /></a>
+      </div>
+      <div v-if="icon" class="info-window-icon-wrap">
+        <IconSvg
+          :iconIndex="props.icon"
+          class="info-window-icon"
+        />
+      </div>
+    </div>
+  </div>
+</template>
