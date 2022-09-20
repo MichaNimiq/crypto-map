9ec569efe987ef15b4ae43b29e834c8e7a253d64
Max <onmax@users.noreply.github.com>
2022-09-20 09:38:36 +0000

    added api generator

---
+++ src/api/models/index.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CryptoCurrency';
+export * from './CryptoLocation';
+export * from './GeoLocation';
+export * from './Link';
+export * from './Pickup';
+export * from './PickupPlaceInformation';
+export * from './PickupPlaceInformationAddressComponentsInner';
+export * from './PickupPlaceInformationGeometry';
+export * from './PickupPlaceInformationGeometryLocation';
+export * from './PickupPlaceInformationGeometryViewport';
+export * from './PickupPlaceInformationPhotosInner';
+export * from './PickupPlaceInformationPlusCode';
+export * from './SearchLocationsResponse';
