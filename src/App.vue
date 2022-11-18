79cbd233d134a7d61117b726847af9147a20ddcf
maximo <maximo@nimiq.com>
2022-11-18 14:15:23 -0600

    added transitions between pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-	<RouterView />
+	<router-view v-slot="{ Component, route }">
+		<transition :name="route.meta.transition">
+			<component :is="Component" />
+		</transition>
+	</router-view>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from "vue-router"
 </script>
+
+<style scoped lang="scss">
+.slide-left-leave-active,
+.slide-left-enter-active {
+	@apply transition-transform;
+}
+
+.slide-left-enter {
+	transform: translate(100%, 0);
+}
+
+.slide-left-leave-to {
+	transform: translate(-100%, 0);
+}
+
+.slide-right-leave-active,
+.slide-right-enter-active {
+	@apply transition-transform;
+}
+
+.slide-right-enter {
+	transform: translate(-100%, 0);
+}
+
+.slide-right-leave-to {
+	transform: translate(200%, 0);
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -11,6 +11,7 @@ export const router = createRouter({
       path: "/establishment/add",
       name: "add_establishment",
       component: () => import("@/components/forms/NewCandidate.vue"),
+      meta: { transition: 'slide-left' },
     },
     {
       path: "/establishment/:id",
@@ -21,6 +22,7 @@ export const router = createRouter({
       path: "/establishment/:id/report",
       name: "report_establishment",
       component: () => import("@/components/forms/ReportEstablishment.vue"),
+      meta: { transition: 'slide-left' },
     },
     {
       path: "/@:lat(-\?\\d\+\.\?\\d\+\?),:lng(-\?\\d\+\.\?\\d\+\?),:zoom(\\d\+)z",
@@ -30,6 +32,17 @@ export const router = createRouter({
     {
       path: "/",
       component: MapLayout,
+      name: 'map'
     },
   ],
 });
+
+router.afterEach((to, from) => {
+  const mapViews = ['map', 'coords', 'establishment_detail'];
+  const formViews = ['add_establishment', 'report_establishment'];
+  if (to.name && typeof to.name === 'string' && mapViews.includes(to.name) &&
+    from.name && typeof from.name === 'string' && formViews.includes(from.name)) {
+    console.log('map slideing rig');
+    to.meta.transition = 'slide-right';
+  }
+})
