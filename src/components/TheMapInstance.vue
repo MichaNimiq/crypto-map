09e007af8fd028240c5577536b83f47c16cf2327
maximo <maximo@nimiq.com>
2022-12-01 15:15:51 +0000

    improved behaviour when loading for first time: - It will show the map directly where the user is - it the url is /establishment/id, it will be redirected

--- src/components/TheMapInstance.vue
+++ src/components/TheMapInstance.vue
@@ -5,17 +5,15 @@ import { useApi } from "@/stores/api"
 import { useApp } from "@/stores/app"
 import { useMap } from "@/stores/map"
 import { Cluster, SuperClusterAlgorithm } from "@googlemaps/markerclusterer"
-import { useDebounceFn } from "@vueuse/shared"
 import { storeToRefs } from "pinia"
-import { onMounted, ref } from "vue"
-import { useRoute } from "vue-router"
+import { ref } from "vue"
 import { CustomMarker, GoogleMap, MarkerCluster } from "vue3-google-map"
+
 
 const apiStore = useApi()
 const { establishmentsInView } = storeToRefs(apiStore)
 
 const appStore = useApp()
-const { goToPlaceId } = appStore
 const { selectedEstablishmentId } = storeToRefs(appStore)
 
 const mapStore = useMap()
@@ -23,43 +21,18 @@ const {
 	navigateToUserLocation,
 	increaseZoom,
 	decreaseZoom,
-	setCenter,
-	setZoom,
 	computeBoundingBox,
 } = mapStore
 const { center, zoom, map$ } = storeToRefs(mapStore)
-
-const route = useRoute()
 
 const googleMapsKey = import.meta.env.VITE_GOOGLE_MAP_KEY
 
 const markers$ = ref<typeof CustomMarker[]>([])
 
-onMounted(async () => {
-	const { lat, lng, zoom: zoomLevel } = route.params
-	const latOk = lat && typeof lat === "string" && !isNaN(Number(lat))
-	const lngOk = lng && typeof lng === "string" && !isNaN(Number(lng))
-	const zoomOk = zoomLevel && typeof zoomLevel === "string" && !isNaN(Number(zoomLevel))
-	if (latOk && lngOk && zoomOk) {
-		setCenter({ lat: Number(lat), lng: Number(lng) })
-		setZoom(Number(zoomLevel))
-	} else {
-		navigateToUserLocation()
-	}
-})
-
-const debouncer = useDebounceFn(computeBoundingBox, 2000)
-
 // We have to wait until GoogleMap component is mounted in order to compute
 // the bounding box of the map if user is accessing the map from an establishment
 function onIdle() {
-	const { place_id: placeId } = route.params
-	const placeIdOk = placeId && typeof placeId === "string"
-	if (placeIdOk) {
-		goToPlaceId(placeId)
-	} else {
-		debouncer()
-	}
+	computeBoundingBox({ updateRoute: false })
 }
 
 const superClusterAlgorithm = new SuperClusterAlgorithm({ radius: 160, maxZoom: 18 }) as unknown as undefined // To avoid lint error
@@ -77,9 +50,9 @@ const render = (cluster: Cluster) => {
 </script>
 
 <template>
-	<GoogleMap v-if="center.lat !== 0 && center.lng !== 0" ref="map$" :api-key="googleMapsKey" class="w-full h-full"
-		:center="center" :zoom="zoom" disable-default-ui :clickable-icons="false" :styles="googleMapStyles"
-		@dragend="computeBoundingBox" @zoom_changed="computeBoundingBox" @idle.once="onIdle">
+	<GoogleMap v-if="center && center.lat !== 0 && center.lng !== 0" ref="map$" :api-key="googleMapsKey"
+		class="w-full h-full" :center="center" :zoom="zoom" disable-default-ui :clickable-icons="false"
+		:styles="googleMapStyles" @dragend="computeBoundingBox" @zoom_changed="computeBoundingBox" @idle.once="onIdle">
 		<MarkerCluster :options="{ algorithm: superClusterAlgorithm, renderer: { render } }">
 			<CustomMarker ref="markers$" v-for="[, establishment] in establishmentsInView" :key="establishment.id"
 				:options="{ position: establishment.geoLocation, anchorPoint: 'TOP_CENTER' }">
