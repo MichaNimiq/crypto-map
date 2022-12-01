67b9a3db315a14ef8b7e7622065d518282e4c022
maximo <maximo@nimiq.com>
2022-12-01 21:28:37 +0000

    added go back button in the forms

--- src/components/forms/FormContainer.vue
+++ src/components/forms/FormContainer.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Button from "@/components/elements/Button.vue"
 import ArrowLinkIcon from "@/components/icons/icon-arrow-link.vue"
+import ArrowLeftIcon from "@/components/icons/icon-arrow-left.vue"
 import { useCaptcha } from "@/composables/useCaptcha"
 import { computed, onMounted, onUnmounted, ref, useSlots } from "vue"
 
@@ -55,8 +56,20 @@ const hasSlot = (name: string) => {
 </script>
 
 <template>
+	<header
+		class="p-6 shadow-[0px_7px_8.5px_rgba(31,_35,_72,_0.04),_0px_2px_2.5px_rgba(31,_35,_72,_0.02)] flex gap-x-4 items-center">
+		<img src="@/assets/logo.svg" alt="Crypto Map logo" class="h-6" />
+		<Button href="/" bg-color="grey">
+			<template #icon>
+				<ArrowLeftIcon class="" />
+			</template>
+			<template #text>
+				{{ $t('Back_to_the_Map') }}
+			</template>
+		</Button>
+	</header>
 	<template
-		class="flex flex-col h-full justify-center md:text-center w-[clamp(284px,768px,calc(100vw-3rem))] mx-auto min-h-screen">
+		class="flex flex-col h-full justify-center md:text-center w-[clamp(284px,768px,calc(100vw-3rem))] mx-auto min-h-[calc(100vh-80px)]">
 		<transition mode="out-in" enter-active-class="transition duration-500 lg:duration-100 ease-out" :enter-from-class="`opacity-0 ${state === FormState.Initial ? '-translate-x-12' : 'translate-x-12'
 		}`" enter-to-class="translate-x-0 opacity-100" leave-active-class="transition duration-300 ease-in"
 			leave-from-class="translate-x-0 opacity-100" :leave-to-class="`opacity-0 ${state === FormState.Initial ? 'translate-x-12' : '-translate-x-12'
