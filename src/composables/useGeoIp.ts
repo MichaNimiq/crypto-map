3364f99c98f1aed380705c2ad005b9f196444c09
maximo <maximo@nimiq.com>
2022-12-14 21:59:52 +0000

    calculate the zoom level given accuracy from geoip

--- src/composables/useGeoIp.ts
+++ src/composables/useGeoIp.ts
@@ -1,4 +1,4 @@
-import { SANTA_TERESA_COORDS, type Point } from "../stores/map";
+import { DEFAULT_ZOOM, SANTA_TERESA_COORDS, type Point } from "../stores/map";
 
 export type GeoIpResponse = {
     location?: {
@@ -8,16 +8,22 @@ export type GeoIpResponse = {
     country?: string,
     city?: string,
     city_names?: { [language: string]: string }, // eslint-disable-line camelcase
+    accuracy_radius?: number, // eslint-disable-line camelcase
+}
+
+export interface IpLocation {
+    location: Point,
+    zoom: number,
 }
 
 const CACHE_MAX_SIZE = 1000;
-const cacheStore = new Map<string, Point>();
+const cacheStore = new Map<string, IpLocation>();
 
 function cached(host: string) {
     return cacheStore.get(host);
 }
 
-function cache(host: string, location: Point) {
+function cache(host: string, location: IpLocation) {
     // Clear cache
     while (cacheStore.size > CACHE_MAX_SIZE) {
         // Don't remove own location
@@ -28,7 +34,7 @@ function cache(host: string, location: Point) {
     cacheStore.set(host, location);
 }
 
-async function locate(host = ''): Promise<Point> {
+async function locate(host = ''): Promise<IpLocation> {
     const cachedResponse = cached(host);
     if (cachedResponse) return cachedResponse;
     const url = new URL(`https://geoip.nimiq-network.com:8443/v1/locate`);
@@ -37,14 +43,24 @@ async function locate(host = ''): Promise<Point> {
     const response = await fetch(url)
     const json: GeoIpResponse = await response.json()
 
-    if (!json || !json.location || !json.location.latitude || !json.location.longitude) return SANTA_TERESA_COORDS
-    const location = {
-        lat: parseFloat(json.location.latitude) || SANTA_TERESA_COORDS.lat,
-        lng: parseFloat(json.location.longitude) || SANTA_TERESA_COORDS.lat
+    if (!json || !json.location || !json.location.latitude || !json.location.longitude)
+        return { location: SANTA_TERESA_COORDS, zoom: DEFAULT_ZOOM }
+
+    // estimate appropiate zoom level based on accuracy radius
+    // with accuracy radius of 5, a good zoom level is 13
+    // with accuracy radius of 50, a good zoom level is 9
+    const zoom = Math.round(Math.log2(1 / (json.accuracy_radius || 25)) + 15);
+
+    const ipLocation: IpLocation = {
+        location: {
+            lat: parseFloat(json.location.latitude) || SANTA_TERESA_COORDS.lat,
+            lng: parseFloat(json.location.longitude) || SANTA_TERESA_COORDS.lat
+        },
+        zoom
     }
-    console.log(`Located ${host} at ${location.lat}, ${location.lng}`);
-    cache(host, location);
-    return location;
+    console.log(`Located ${host} at ${ipLocation.location.lat}, ${ipLocation.location.lng}`);
+    cache(host, ipLocation);
+    return ipLocation;
 }
 
 export function useGeoIp() {
