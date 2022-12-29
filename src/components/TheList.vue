45c27c788f91a8604721905da0bd4dd5226d808e
maximo <maximo@nimiq.com>
2022-12-29 20:25:51 +0100

    added 'Show more establishment' button in the list - user can explore new establishment using the button in the list - all establishments structures in one store - added nearEstablishmentsNotInView

--- src/components/TheList.vue
+++ src/components/TheList.vue
@@ -5,7 +5,7 @@ import ChevronLeftIcon from "@/components/icons/icon-arrow-small-left.vue"
 import NoEstablishments from "@/components/illustrations/no-establishments.vue"
 import ListIcon from "@/components/icons/icon-list.vue"
 import { useBreakpoints } from "@/composables/useBreakpoints"
-import { useApi } from "@/stores/api"
+import { useEstablishments } from "@/stores/establishments"
 import { useApp } from "@/stores/app"
 import { storeToRefs } from "pinia"
 import { computed, ref, watch } from "vue"
@@ -17,8 +17,8 @@ const scroller$ = ref<HTMLDivElement>()
 const appStore = useApp()
 const { listIsShown, selectedEstablishmentUuid } = storeToRefs(appStore)
 
-const apiStore = useApi()
-const { establishmentsInView } = storeToRefs(apiStore)
+const establishmentsStore = useEstablishments()
+const { establishmentsInView, nearEstablishmentsNotInView, shouldShowNearby } = storeToRefs(establishmentsStore)
 
 const listIsEmpty = computed(() => establishmentsInView.value.length === 0)
 
@@ -42,20 +42,44 @@ function slideTo(index: number, behavior: "smooth" | "auto" = "smooth") {
 	<div
 		class="xl:flex xl:gap-x-6 absolute max-xl:transition-all xl:transition-transform-width max-xl:bottom-0 overflow-auto scroll-space max-xl:bg-white max-xl:shadow max-xl:w-screen"
 		:class="{
-			'xl:-translate-x-96 max-xl:top-full': !listIsShown,
-			'h-full lg:h-[calc(100vh-80px)]': !listIsEmpty,
-			'lg:h-[calc(100vh-80px)]': listIsEmpty,
-		}">
-		<ul ref="scroller$"
-			class="xl:w-96 p-6 columns-2xs gap-x-6 space-y-6 snap-y snap-mandatory scroll-py-6 bg-white xl:shadow overflow-y-auto scroll-space z-2 relative max-xl:pb-16"
-			v-if="!listIsEmpty">
-			<li v-for="establishment in establishmentsInView" :key="establishment.uuid"
-				class="list-item-wrap xl:snap-start shadow-lg border pt-1.5 pb-6 rounded-lg flex flex-col break-inside-avoid-column transition-[box-shadow]"
-				:class="{ 'ring ring-ocean': establishment.uuid === selectedEstablishmentUuid }"
-				:data-establishment-id="establishment.uuid">
-				<EstablishmentCard :establishment="establishment" />
-			</li>
-		</ul>
+	'xl:-translate-x-96 max-xl:top-full': !listIsShown,
+	'h-full lg:h-[calc(100vh-80px)]': !listIsEmpty,
+	'lg:h-[calc(100vh-80px)]': listIsEmpty,
+}">
+		<div v-if="!listIsEmpty"
+			class="xl:w-96 p-6 columns-2xs gap-x-6 space-y-6 snap-y snap-mandatory scroll-py-6 bg-white xl:shadow overflow-y-auto scroll-space z-2 relative max-xl:pb-16">
+			<ul ref="scroller$">
+				<li v-for="establishment in establishmentsInView" :key="establishment.uuid"
+					class="list-item-wrap xl:snap-start shadow-lg border pt-1.5 pb-6 rounded-lg flex flex-col break-inside-avoid-column transition-[box-shadow]"
+					:class="{ 'ring ring-ocean': establishment.uuid === selectedEstablishmentUuid }"
+					:data-establishment-id="establishment.uuid">
+					<EstablishmentCard :establishment="establishment" />
+				</li>
+
+				<template v-if="shouldShowNearby">
+					<li v-for="establishment in nearEstablishmentsNotInView" :key="establishment.uuid"
+						class="list-item-wrap xl:snap-start shadow-lg border pt-1.5 pb-6 rounded-lg flex flex-col break-inside-avoid-column transition-[box-shadow]"
+						:class="{ 'ring ring-ocean': establishment.uuid === selectedEstablishmentUuid }"
+						:data-establishment-id="establishment.uuid">
+						<EstablishmentCard :establishment="establishment" />
+					</li>
+				</template>
+			</ul>
+
+			<Button bgColor="grey" class="!mt-9" size="md" @click="establishmentsStore.showNearby"
+				v-if="!shouldShowNearby && nearEstablishmentsNotInView.length > 0">
+				<template #icon>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="w-3 h-3 text-space">
+						<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M1 8h14M8 1v14" />
+					</svg>
+				</template>
+
+				<template #text>
+					{{ $t("Show_more_establishments") }}
+				</template>
+			</Button>
+
+		</div>
 
 		<div v-else
 			class="grid place-content-center p-6 w-screen xl:w-96 bg-white items-center gap-6 max-xl:py-20 xl:shadow xl:h-main">
@@ -71,9 +95,9 @@ function slideTo(index: number, behavior: "smooth" | "auto" = "smooth") {
 					@click="appStore.toggleList()">
 					<template #icon>
 						<component :is="listIsShown ? ChevronLeftIcon : ListIcon" class="text-space h-4" :class="{
-							'w-4.5': listIsShown,
-							'w-4': !listIsShown,
-						}" />
+	'w-4.5': listIsShown,
+	'w-4': !listIsShown,
+}" />
 					</template>
 
 					<template #text v-if="!smallScreen">
